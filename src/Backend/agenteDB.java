733e93e3606a9df4d7610e2242fd9db5a3f8f882
danigcj31 <danigcj21@gmail.com>
2020-10-07 12:32:53 +0200

    error al conectar con mongodb

--- src/Backend/agenteDB.java
+++ src/Backend/agenteDB.java
@@ -1,55 +1,24 @@
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.Mongo;
+import org.bson.Document;
 
-import Prueba.Connection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.types.ObjectId;
 
-import java.net.*;
-import java.util.logging.Level;
-import java.util.logging.logger;
+public class agenteDB {
 
-public class agente {
-	DB BaseDatos;
-	DBCollection coleccion;
-	BasicDBObject document = new BasicDBObject();
-	
-	public Connection() {
-		try {
-			Mongo mongo = new Mongo("localhost",27017);
-			BaseDatos = mongo.getDB("ToDo");
-			coleccion = BaseDatos.getCollection("ToDo");
-			System.out.println("Conectado");
-		} catch (Exception e) {
-			// TODO: handle exception
-			logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null,ex);
-		}
-	}
-	public void mostrar() {
-		DBCursor cursor = coleccion.find();
-		while(cursor.hasNext()) {
-			System.out.println(cursor.next());		
-		}
-	}
-	public boolean insertar(String accion) {
-		document.put("accion",accion);
-		coleccion.insert(document);
-		return true;
-	}
-	public boolean actualizar(String accion) {
-		document.put("accion",accionVieja);
-		BasicDBObject documentNuevo = new BasicDBObject();
-		documentNuevo.put("accion",accionNueva);
-		coleccion.findAndMOdify(document, documentNuevo);
-		return true;
+	public static void main(String args[]) {
+		MongoClientURI uri = new MongoClientURI("mongodb+srv://david:david123@clusterequipo1.idibk.mongodb.net/<dbname>?retryWrites=true&w=majority");
 		
-		
+		try(MongoClient mongoClient = new MongoClient(uri)){
+				
+			MongoDatabase database = mongoClient.getDatabase("BD_Equipo1");
+			MongoCollection<Document> collection = database.getCollection("User");
+			Document query = new Document("_id", new ObjectId("5f7d911ca1d9414ac992387a"));
+			Document result = collection.find(query).iterator().next();
+			
+			System.out.println("nombre: " + result.getString("nombre"));
+			}
 	}
-	public boolean eliminar(String accion) {
-		document.put("accion",accion);
-		coleccion.remove(document);
-		return true;
-	}
-
 }
