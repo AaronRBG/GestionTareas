8f8fb3056bab187931e0d911ba9b29a4f2a4f9f0
victo <victo@DESKTOP-46DDDQ9>
2020-10-09 18:51:20 +0200

    Intento de inserts

--- src/main/java/com/app/gestionTareas/dominio/Tarea.java
+++ src/main/java/com/app/gestionTareas/dominio/Tarea.java
@@ -6,23 +6,13 @@ public class Tarea {
 
 	private String nombre;
 	private boolean done;
+	private String id;
 	
 	public Tarea(String nombre){
 		this.nombre = nombre;
 		this.done = false;
 	}
 	
-	private void setDone(boolean done) {
-		this.done = done;
-	}
-	
-	private String getNombre() {
-		return this.nombre;
-	}
-	
-	private Boolean getDone() {
-		return this.done;
-	}
 	
     public JSONObject toJSON() {
         JSONObject jso = new JSONObject();
--- src/main/java/com/app/gestionTareas/persistencia/TareaDAO.java
+++ src/main/java/com/app/gestionTareas/persistencia/TareaDAO.java
@@ -1,66 +1,61 @@
 package com.app.gestionTareas.persistencia;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.app.gestionTareas.dominio.Tarea;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
 import com.mongodb.client.MongoCollection;
+
 
 @Repository
 public interface TareaDAO extends CrudRepository<Tarea, String> {
 	
-	public static void insert(String nombre, Boolean done) {
-		
-		int donette = 0;
-		if(done)
-			donette=1;
-		
-		try (MongoCollection<Document> bd = agenteDB.get().getBd()) {
-			String sql = "insert into Tareas (nombre, done) values (?, ?)";
-			try (PreparedStatement ps = bd.prepareStatement(sql)) {
-				ps.setString(1, nombre);
-				ps.setString(2, Integer.toString(donette));
-				ps.executeUpdate();
-			}
-			bd.close();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+	public static void insertar(String nombre, boolean done) {
+		Document document;
+		int c=0;
+		if (done) {
+			c=1;
 		}
+		MongoCollection <Document> coleccion = agenteDB.get().getBd();
+		document = new Document();
+		document.put("nombre",nombre);
+		document.put("done",c);
+		coleccion.insertOne(document);
+
 	}
+	public static void actualizar(String nombre, boolean done, String id) {
+		Document document;
+		Document document2;
+		int c=0;
+		if (done) {
+			c=1;
+		}
+		MongoCollection <Document> coleccion = agenteDB.get().getBd();
+		
+		
+		document = new Document("_id",new ObjectId(id));
+		document2 = new Document();
+		
+		document2.put("_id",new ObjectId(id));
+		
+		document2.put("nombre",nombre);
+		document2.put("done",c);
+		
+		coleccion.updateOne(document, document);
+		
+		
+		
+	}
+	public static void eliminar(String accion) {
+		document.put("accion",accion);
+		coleccion.remove(document);
+		return true;
+	}
+		
 	
-	public static void update(String nombre, Boolean done) {
-		
-		int donette = 0;
-		if(done)
-			donette=1;
-		
-		try (Connection bd = agenteDB.get().getBd()) {
-			String sql = "update Tareas set done = ? where nombre = ?";
-			try (PreparedStatement ps = bd.prepareStatement(sql)) {
-				ps.setString(2, nombre);
-				ps.setString(1, Integer.toString(donette));
-				ps.executeUpdate();
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
-	
-	public static void delete(String nombre) {
-				
-		try (Connection bd = agenteDB.get().getBd()) {
-			String sql = "delete from Tareas where nombre = ?";
-			try (PreparedStatement ps = bd.prepareStatement(sql)) {
-				ps.setString(1, nombre);
-				ps.executeUpdate();
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
 	
 }
